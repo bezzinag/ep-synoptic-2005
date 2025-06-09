e081086d26e72714b66654c380e4b30c746fb544
bezzinag <bezzinag@yahoo.com>
2025-06-09 16:07:09 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ep_synoptic_2005.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
