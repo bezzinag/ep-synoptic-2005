90a4b0384172852fdd84de1ed5287aed4b9a62f1
bezzinag <bezzinag@yahoo.com>
2025-06-09 16:28:28 +0200

    created upload file model

---
+++ Models/UploadFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations; // needed because i am using the display attrib from .DataAnnotations
+
+namespace ep_synoptic_2005.Models
+{
+    public class UploadFile
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "File Title")] // like this Title will show as a nice "File title"
+        public string Title { get; set; }
+
+        public string StoredFileName { get; set; }
+
+        public string UploadedByUserId { get; set; }
+
+        public DateTime UploadedDate { get; set; }
+
+    }
+}
